b3c8fc879b078881c1dafa548df17f5df2335dac
Catherine Salazar <catcreates13@gmail.com>
2018-09-19 01:02:31 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,8 +3,11 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
+    def get_first_song
+    self.songs.first
   end
+end
+
 
   def song_count
     #return the number of songs associated with the artist
